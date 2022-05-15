5b59d6279ca996b71ef4ec6d225e3338961c3572
CRAK1120 <noguchiakiracr71120@gmail.com>
2022-05-15 21:13:49 +0900

    疑似コード加えた

--- CKY-Parser_1.py
+++ CKY-Parser_1.py
@@ -20,100 +20,86 @@ V->like:0.4
 P->like:1.0
 D->an:1.0"""
 
-class CKY:
-    def __init__(self, grammar_text):
-        self.grammar_dict = defaultdict(set)
-        for line in grammar_text.split("\n"):
-            rule, p = line.split(":")
-            l, r = rule.split("->")
-            self.grammar_dict[r].add((l, float(p)))
 
-        self.cky_array = None
+def CKY(leaves, lexical_rule, syntax_rule):
+    n = len(leaves)
+    cell = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
+    for i, leaf in enumerate(leaves):
+        cell[i][i + 1] = lexical_rule[leaf]
 
-    def _init_cky_array(self, length):
-        self.cky_array = [[[] for _ in range(length)]
-                          for i in range(length)]
-        return self.cky_array
+    for l in range(2, n + 1):
+        for i in range(n - l + 1):
+            j = i + l
+            for k in range(i + 1, j):
+                for prob_l, x_l in cell[i][k]:
+                    for prob_r, x_r in cell[k][j]:
+                        cell[i][j] += syntax_rule.get((x_l, x_r), [])
+    return cell
 
-    def parse(self, text):
-        words = text.split(" ")
-        self.length = len(words)
-        self._init_cky_array(self.length)
 
-        for i, word in enumerate(words):
-            for l, p in self.grammar_dict[word]:
-                self.cky_array[i][i].append((l, word, p))
-        
-        # 対角成分の1つ右,2つ右,...と処理を回すループ
-        for d in range(1, self.length):
-            
-            # 斜め下に進んでいくループ
-            # i,jでどのセルを処理対象とするか決める
-            for i in range(self.length - d):
-                j = i + d
-                
-                # セルの中身を埋めるループ
-                for k in range(i, j):
+def parse(self, text):
+    words = text.split(" ")
+    self.length = len(words)
+    self._init_cky_array(self.length)
 
-                    # 右辺の可能な組み合わせを列挙してる
-                    for a, b in product(
-                            range(len(self.cky_array[i][k])),
-                            range(len(self.cky_array[k+1][j]))):
+    for i, word in enumerate(words):
+        for l, p in self.grammar_dict[word]:
+            self.cky_array[i][i].append((l, word, p))
 
-                        # 辞書のキーを作る
-                        s = "{0} {1}".format(
-                            self.cky_array[i][k][a][0],
-                            self.cky_array[k+1][j][b][0])
-                        
-                        # キーに合致する文法をぜんぶ出す
-                        for l,p in self.grammar_dict[s]:
+    # 対角成分の1つ右,2つ右,...と処理を回すループ
+    for d in range(1, self.length):
 
-                            # セルに中身を入れる
-                            self.cky_array[i][j].append(
-                                (l, ((i,k,a), (k+1,j,b)), p))
+        # 斜め下に進んでいくループ
+        # i,jでどのセルを処理対象とするか決める
+        for i in range(self.length - d):
+            j = i + d
 
-        # parseの最後
-        return self._gen_xml_etree_list()
+            # セルの中身を埋めるループ
+            for k in range(i, j):
 
-    def _traverse_tree(self, index=(0,0,0)):
-        # 構文木を辿る
-        i,j,k = index
-        node = self.cky_array[i][j][k]
-        elem = etree.Element(node[0])
-        child = node[1]
-        p = node[2]
-        elem.attrib["p"] = str(p)
+                # 右辺の可能な組み合わせを列挙してる
+                for a, b in product(range(len(self.cky_array[i][k])),
+                                    range(len(self.cky_array[k + 1][j]))):
 
-        if type(child) == str:
-            elem.text = child
-            return elem
-        else:
-            l, r = child
-            elem.append(self._traverse_tree(index=l))            
-            elem.append(self._traverse_tree(index=r))
-            return elem
+                    # 辞書のキーを作る
+                    s = "{0} {1}".format(self.cky_array[i][k][a][0],
+                                         self.cky_array[k + 1][j][b][0])
 
-    def _gen_xml_etree_list(self):
-        # 再帰呼出しを開始する
-        lst = []
-        for i, s in enumerate(self.cky_array[0][self.length - 1]):
-            if s[0] != "S":
-                pass
-            else:
-                # etreeのまま返すことにしよう...
-                lst.append(self._traverse_tree((0,4,i)))
-        return lst
+                    # キーに合致する文法をぜんぶ出す
+                    for l, p in self.grammar_dict[s]:
 
-def main():
-    cky = CKY(grammar_text)
-    lst = cky.parse(example_sentence)
-    for xml_tree in lst:
-        p = 1
-        for elem in xml_tree.iter():
-            p *= float(elem.attrib["p"])
-        print(p)
-        print(
-            etree.tostring(xml_tree, pretty_print=True).decode())
+                        # セルに中身を入れる
+                        self.cky_array[i][j].append(
+                            (l, ((i, k, a), (k + 1, j, b)), p))
+
+    # parseの最後
+    return self._gen_xml_etree_list()
+
 
 if __name__ == "__main__":
-    main()
+    from nltk.tree import Tree
+    from pathlib import Path as p
+    tree: Tree = Tree.fromstring(p("example/tree.mrg").read_text())
+
+    from pprint import pprint
+    from counter import rule_as_dict
+
+    lexical_dict = {
+        'Time': [(0.5, 'N')],
+        'an': [(1.0, 'D')],
+        'arrow': [(0.5, 'N')],
+        'flies': [(1.0, 'V')],
+        'like': [(1.0, 'P')]
+    }
+
+    syntax_dict = {
+        ('D', 'N'): [(0.5, 'NP')],
+        ('N', ): [(0.5, 'NP')],
+        ('NP', 'VP'): [(1.0, 'S')],
+        ('P', 'NP'): [(1.0, 'PP')],
+        ('V', 'PP'): [(1.0, 'VP')]
+    }
+
+    pprint(syntax_dict)
+
+    print(*CKY(tree.leaves(), lexical_dict, syntax_dict), sep="\n")
