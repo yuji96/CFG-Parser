ca8567e676eefb425c912b805c6ad305726a6b1f
yuji96 <y.yama683@gmail.com>
2022-05-23 21:50:10 +0900

    make predict function

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
 import pickle
 from pathlib import Path
+from random import sample
+from time import time
 
 from nltk.tree import Tree
 from tqdm import tqdm
@@ -7,33 +9,43 @@ from tqdm import tqdm
 from cky import CKY, build_tree
 from reader import read_cleaned_corpus
 
-lexical_dict = pickle.loads(Path("stats/lexical_dict.pkl").read_bytes())
-syntax_dict = pickle.loads(Path("stats/syntax_dict.pkl").read_bytes())
-unary_dict = pickle.loads(Path("stats/unary_dict.pkl").read_bytes())
+pwd = Path(__file__).parent
+lexical_dict = pickle.loads(pwd.joinpath("stats/lexical_dict.pkl").read_bytes())
+syntax_dict = pickle.loads(pwd.joinpath("stats/syntax_dict.pkl").read_bytes())
+unary_dict = pickle.loads(pwd.joinpath("stats/unary_dict.pkl").read_bytes())
 
-golds = read_cleaned_corpus("debug")
-preds = []
-for gold in tqdm(golds.copy()):
-    gold: Tree
 
+def predict(gold: Tree) -> Tree:
+    a = time()
     gold.chomsky_normal_form()
-    chart, backpointer = CKY(gold.leaves(), lexical_dict, syntax_dict, unary_dict,
-                             beam=30)
+    # lexical_dict, syntax_dict, unary_dict = rule_as_dict(gold.productions())
+    _, backpointer = CKY(gold.leaves(), lexical_dict, syntax_dict, unary_dict,
+                         beam=25)
+    b = time()
     pred = build_tree(backpointer)
-
-    gold.un_chomsky_normal_form()
     pred.un_chomsky_normal_form()
-    preds.append(pred)
+    c = time()
+    tqdm.write(repr((b - a, c - b)))
+    return pred
 
-Path("tmp/gold.txt").write_text("\n".join(
-    [tree.pformat(margin=float("inf")) for tree in golds]))
-Path("tmp/pred.txt").write_text("\n".join(
-    [tree.pformat(margin=float("inf")) for tree in preds]))
 
-# 可視化用
-# chart_count = [[len(tags) for tags in row] for row in chart]
-# plt.imshow(chart_count)
-# plt.show()
-# pprint(chart[0][-1])
-# visible_print(chart)
-# pred_tree.pretty_print()
+if __name__ == "__main__":
+    golds = sample(read_cleaned_corpus("test"), 10)
+    # golds = read_cleaned_corpus("debug")
+    Path("tmp/gold.txt").write_text("\n".join(
+        [tree.pformat(margin=float("inf")) for tree in golds]))
+    preds = []
+    for gold in tqdm(golds.copy()):
+        pred = predict(gold)
+        preds.append(pred)
+
+    Path("tmp/pred.txt").write_text("\n".join(
+        [tree.pformat(margin=float("inf")) for tree in preds]))
+
+    # 可視化用
+    # chart_count = [[len(tags) for tags in row] for row in chart]
+    # plt.imshow(chart_count)
+    # plt.show()
+    # pprint(chart[0][-1])
+    # visible_print(chart)
+    # pred_tree.pretty_print()
